ee95c39591ff120f28c1fda63fddfcb9fc08b189
Xun Sun <xusun@microsoft.com>
2017-04-19 17:27:45 -0700

    Update pymssql.sh

--- pymssql.sh
+++ pymssql.sh
@@ -1,1 +1,4 @@
-pip install pymssql
+sudo apt-get --assume-yes update  
+sudo apt-get --assume-yes install freetds-dev freetds-bin  
+sudo apt-get --assume-yes install python-dev python-pip  
+sudo pip install pymssql  
