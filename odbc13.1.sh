17e3092a5a93381659ab74072e75350e18b271e8
Xun Sun <xusun@microsoft.com>
2017-04-20 00:16:44 -0700

    Create odbc13.1.sh

---
+++ odbc13.1.sh
@@ -0,0 +1,3 @@
+sudo su 
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
