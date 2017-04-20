fc7aa631faa3f452613aab776a55c82d539d52a8
Xun Sun <xusun@microsoft.com>
2017-04-20 14:07:07 -0700

    Update pyodbc.sh

--- pyodbc.sh
+++ pyodbc.sh
@@ -7,5 +7,5 @@ curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sou
 exit
 sudo dpkg --configure -a
 sudo apt-get update
+sudo -H pip install pyodbc
 sudo ACCEPT_EULA=Y apt-get --assume-yes install msodbcsql=13.1.4.0-1 mssql-tools=14.0.3.0-1 unixodbc-dev
-sudo -H pip install pyodbc==3.1.1
