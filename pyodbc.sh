7b733d7f366338ca1d61e6393d6a7e70dd918506
Xun Sun <xusun@microsoft.com>
2017-04-20 18:22:24 -0700

    Update pyodbc.sh

--- pyodbc.sh
+++ pyodbc.sh
@@ -7,7 +7,7 @@ echo 'su' >>/tmp/pyodbcinstall.log
 sudo dpkg --configure -a
 echo 'dpkg --config' >>/tmp/pyodbcinstall.log
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql.list
+curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
 echo 'curled' >>/tmp/pyodbcinstall.log
 echo `date` >>/tmp/pyodbcpackage.log
 echo 'downloaded' >>/tmp/pyodbcpackage.log
@@ -15,11 +15,11 @@ sudo dpkg --configure -a
 #sudo apt-get update
 echo 'updated' >>/tmp/pyodbcinstall.log
 echo start install pyodbc >>/tmp/pyodbcinstall.log
-echo `date` >>/tmp/pyodbc.log
+#echo `date` >>/tmp/pyodbc.log
 sudo -H pip install pyodbc >>/tmp/pyodbcinstall.log
 
 echo start install msodbcsql >>/tmp/pyodbcinstall.log
-echo `date` >>/tmp/msodbcsql.log
+#echo `date` >>/tmp/msodbcsql.log
 echo 'install 13.1' >>/tmp/pyodbcinstall.log
 sudo ACCEPT_EULA=Y apt-get --assume-yes --allow-downgrades install msodbcsql=13.1.6.0-1 mssql-tools=14.0.5.0-1 unixodbc-dev >>/tmp/pyodbcinstall.log
 echo `date` >>/tmp/datelog.txt
