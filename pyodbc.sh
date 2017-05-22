cf2d8a03e46f28b8be13857964a9cc9d25d6d4a3
Xun Sun <xusun@microsoft.com>
2017-05-22 13:22:35 -0700

    Update pyodbc.sh

--- pyodbc.sh
+++ pyodbc.sh
@@ -8,8 +8,9 @@ sudo dpkg --configure -a
 #sudo apt-get update
 sudo -H pip install pyodbc
 
-sudo ACCEPT_EULA=Y apt-get --assume-yes --allow-downgrades install msodbcsql=13.1.6.0-1 mssql-tools=14.0.5.0-1 unixodbc-dev >>/tmp/pyodbcinstall.log
+sudo ACCEPT_EULA=Y apt-get --assume-yes --allow-downgrades install msodbcsql=13.1.6.0-1 mssql-tools=14.0.5.0-1 unixodbc-dev >>/tmp/pyodbcinstall0522.log
 echo 'export PATH="/opt/mssql-tools/bin/:$PATH"' >> ~/.bash_profile
 echo 'export PATH="/opt/mssql-tools/bin/:$PATH"' >> ~/.bashrc
 source ~/.bashrc
-sudo /usr/bin/anaconda/bin/conda install -y -c anaconda pyodbc=3.0.10 >>/tmp/pyodbcinstall.log
+sudo /usr/bin/anaconda/bin/conda install -y -c anaconda pyodbc=3.0.10 >>/tmp/pyodbcinstall0522.log
+sudo /usr/bin/anaconda/bin/conda update -y libgcc >>/tmp/log0522.txt
