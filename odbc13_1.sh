c4fe8330d78c2d45fbc504d9330ef7778b4c6c46
Xun Sun <xusun@microsoft.com>
2017-06-01 09:39:28 -0700

    Update odbc13_1.sh

--- odbc13_1.sh
+++ odbc13_1.sh
@@ -1,4 +1,5 @@
 sudo su 
+pwd >>/tmp/log0601.txt
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
 #exit
