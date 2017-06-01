9a2cb130d4296240a78b622ab0e27253d0f5536a
Xun Sun <xusun@microsoft.com>
2017-06-01 09:35:48 -0700

    Update odbc13_1.sh

--- odbc13_1.sh
+++ odbc13_1.sh
@@ -1,7 +1,7 @@
 sudo su 
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
-exit
+#exit
 sudo ACCEPT_EULA=Y apt-get --assume-yes install msodbcsql=13.1.4.0-1 mssql-tools=14.0.3.0-1 unixodbc-dev >>/tmp/log0601.txt
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
