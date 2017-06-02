0a912518e1890e3c8dea16450ad7776f7da6f368
Xun Sun <xusun@microsoft.com>
2017-06-01 18:18:30 -0700

    Update pyodbc.sh

--- pyodbc.sh
+++ pyodbc.sh
@@ -1,7 +1,8 @@
+sudo apt-get -y update
 sudo su
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
-sudo apt-get -y update
+
 sudo ACCEPT_EULA=Y apt-get --assume-yes install msodbcsql=13.1.6.0-1 mssql-tools=14.0.3.0-1 unixodbc-dev
 
 echo 'export PATH="/opt/mssql-tools/bin/:$PATH"' >> ~/.bash_profile
