ec95c81b0ac1b0a7f60514e71134fc2e194001b3
Xun Sun <xusun@microsoft.com>
2017-06-01 18:27:14 -0700

    Update pyodbc.sh

--- pyodbc.sh
+++ pyodbc.sh
@@ -1,14 +1,11 @@
-sudo apt-get -y update
-sudo su
+#sudo apt-get -y update
+#sudo su
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
-
-sudo ACCEPT_EULA=Y apt-get --assume-yes install msodbcsql=13.1.6.0-1 mssql-tools=14.0.3.0-1 unixodbc-dev
-
+sudo ACCEPT_EULA=Y apt-get --assume-yes install msodbcsql mssql-tools unixodbc-dev
 echo 'export PATH="/opt/mssql-tools/bin/:$PATH"' >> ~/.bash_profile
 echo 'export PATH="/opt/mssql-tools/bin/:$PATH"' >> ~/.bashrc
 source ~/.bashrc
-
 sudo -H pip install pyodbc
 sudo /usr/bin/anaconda/bin/conda install -y -c anaconda pyodbc=3.0.10
 sudo /usr/bin/anaconda/bin/conda update -y libgcc
